551f3a1303d4da34fc5acc5d4df7c5c58169d17c
thomazrlima <thomazlima211@gmail.com>
2025-02-17 13:51:39 +0000

    Versão em linux

--- T1/main.c
+++ T1/main.c
@@ -1,77 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <time.h>
 
 void lerMatriz(FILE *arquivo, long long **matriz, int tamanho);
-void multiplicarMatrizes(long long **A, long long **B, long long **C, long long **D, int tamanho);
+void multiplicarMatrizes(long long **A, long long **B, long long **C, int tamanho);
 void imprimirMatriz(FILE *arquivo, long long **matriz, int tamanho);
 
 double getTime() {
-    LARGE_INTEGER frequency, start;
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&start);
-    return (double)start.QuadPart / frequency.QuadPart;
+    clock_t start = clock();
+    return (double)start / CLOCKS_PER_SEC;
 }
 
 int main(int argc, char *argv[]) {
-    FILE *arquivoEntrada = fopen("C:/PENTES/SPD/T1/entrada.txt", "r");
-    if (!arquivoEntrada) {
-        perror("Erro ao abrir o arquivo de entrada");
+    if (argc != 4) {
+        printf("Uso: %s <arquivoA> <arquivoB> <arquivoSaida>\n", argv[0]);
         return 1;
     }
 
-    FILE *arquivoSaida = fopen("C:/PENTES/SPD/T1/arquivo_saida.txt", "w");
-    if (!arquivoSaida) {
+    FILE *arquivoEntradaA = fopen(argv[1], "r");
+    if (!arquivoEntradaA) {
+        perror("Erro ao abrir o arquivo de entrada A");
+        return 1;
+    }
+
+    FILE *arquivoEntradaB = fopen(argv[2], "r");
+    if (!arquivoEntradaB) {
+        perror("Erro ao abrir o arquivo de entrada B");
+        fclose(arquivoEntradaA);
+        return 1;
+    }
+
+    FILE *C = fopen(argv[3], "w");
+    if (!C) {
         perror("Erro ao criar o arquivo de saída");
-        fclose(arquivoEntrada);
+        fclose(arquivoEntradaA);
+        fclose(arquivoEntradaB);
         return 1;
     }
 
-    FILE *arquivoCSV = fopen("C:/PENTES/SPD/T1/tempo_execucao.csv", "a");
+    FILE *arquivoCSV = fopen("tempo_execucao.csv", "a");
     if (!arquivoCSV) {
         perror("Erro ao criar ou abrir o arquivo CSV");
-        fclose(arquivoEntrada);
-        fclose(arquivoSaida);
+        fclose(arquivoEntradaA);
+        fclose(arquivoEntradaB);
+        fclose(C);
         return 1;
     }
 
     int tamanho;
-    fscanf(arquivoEntrada, "%d", &tamanho);
+    fscanf(arquivoEntradaA, "%d", &tamanho);
 
     long long **A = (long long **)malloc(tamanho * sizeof(long long *));
     long long **B = (long long **)malloc(tamanho * sizeof(long long *));
-    long long **C = (long long **)malloc(tamanho * sizeof(long long *));
-    long long **D = (long long **)malloc(tamanho * sizeof(long long *));
+    long long **matrizC = (long long **)malloc(tamanho * sizeof(long long *));
 
     for (int i = 0; i < tamanho; i++) {
         A[i] = (long long *)malloc(tamanho * sizeof(long long));
         B[i] = (long long *)malloc(tamanho * sizeof(long long));
-        C[i] = (long long *)malloc(tamanho * sizeof(long long));
-        D[i] = (long long *)malloc(tamanho * sizeof(long long));
+        matrizC[i] = (long long *)malloc(tamanho * sizeof(long long));
     }
 
-    lerMatriz(arquivoEntrada, A, tamanho);
-    lerMatriz(arquivoEntrada, B, tamanho);
-    lerMatriz(arquivoEntrada, C, tamanho);
-    fclose(arquivoEntrada);
+    lerMatriz(arquivoEntradaA, A, tamanho);
+    lerMatriz(arquivoEntradaB, B, tamanho);
+    fclose(arquivoEntradaA);
+    fclose(arquivoEntradaB);
 
     double start = getTime();
-    multiplicarMatrizes(A, B, C, D, tamanho);
+    multiplicarMatrizes(A, B, matrizC, tamanho);
     double end = getTime();
 
-    imprimirMatriz(arquivoSaida, D, tamanho);
-    fclose(arquivoSaida);
+    imprimirMatriz(C, matrizC, tamanho);
+    fclose(C);
 
     for (int i = 0; i < tamanho; i++) {
         free(A[i]);
         free(B[i]);
-        free(C[i]);
-        free(D[i]);
+        free(matrizC[i]);
     }
     free(A);
     free(B);
-    free(C);
-    free(D);
+    free(matrizC);
 
     fprintf(arquivoCSV, "%d,%.9f\n", tamanho, end - start);
 
@@ -90,34 +98,15 @@ void lerMatriz(FILE *arquivo, long long **matriz, int tamanho) {
     }
 }
 
-void multiplicarMatrizes(long long **A, long long **B, long long **C, long long **D, int tamanho) {
-    long long **temp = (long long **)malloc(tamanho * sizeof(long long *));
-    for (int i = 0; i < tamanho; i++) {
-        temp[i] = (long long *)malloc(tamanho * sizeof(long long));
-    }
-
+void multiplicarMatrizes(long long **A, long long **B, long long **C, int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         for (int j = 0; j < tamanho; j++) {
-            temp[i][j] = 0;
+            C[i][j] = 0;
             for (int k = 0; k < tamanho; k++) {
-                temp[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
-
-    for (int i = 0; i < tamanho; i++) {
-        for (int j = 0; j < tamanho; j++) {
-            D[i][j] = 0;
-            for (int k = 0; k < tamanho; k++) {
-                D[i][j] += temp[i][k] * C[k][j];
-            }
-        }
-    }
-
-    for (int i = 0; i < tamanho; i++) {
-        free(temp[i]);
-    }
-    free(temp);
 }
 
 void imprimirMatriz(FILE *arquivo, long long **matriz, int tamanho) {
