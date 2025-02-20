93a2df4964e9aa0ae4e2f6bcc25210b231ce6335
thomazrlima <thomazlima211@gmail.com>
2025-02-20 17:17:18 +0000

    Testes do sequencial

--- T1/execute_tests.sh
+++ T1/execute_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 OUTPUT_CSV="tempos_execucao.csv"
-echo "Teste,Tamanho,Real,User,Sys" > $OUTPUT_CSV
 
-for i in {1..15}
+if [ ! -f "$OUTPUT_CSV" ]; then
+    echo "Teste,Tamanho,Real,User,Sys" > "$OUTPUT_CSV"
+fi
+
+for i in {1..1500}
 do
     ARQUIVO_A="A.txt"
     ARQUIVO_B="B.txt"
@@ -11,14 +14,14 @@ do
 
     MATRIX_SIZE=$(wc -l < "$ARQUIVO_A")
 
-    { time ./programa $MATRIX_SIZE $ARQUIVO_A $ARQUIVO_B $ARQUIVO_C; } 2> tempo.txt
+    { time -f "%e" ./programa $MATRIX_SIZE $ARQUIVO_A $ARQUIVO_B $ARQUIVO_C; } 2> tempo.txt
 
     REAL_TIME=$(grep real tempo.txt | awk '{print $2}')
     USER_TIME=$(grep user tempo.txt | awk '{print $2}')
     SYS_TIME=$(grep sys tempo.txt | awk '{print $2}')
 
-    echo "$i,$MATRIX_SIZE,$REAL_TIME,$USER_TIME,$SYS_TIME" >> $OUTPUT_CSV
-    echo "Teste $i: Tamanho=$MATRIX_SIZE, Real=$REAL_TIME, User=$USER_TIME, Sys=$SYS_TIME"
+    echo "$i,$MATRIX_SIZE,$REAL_TIME,$USER_TIME,$SYS_TIME" >> "$OUTPUT_CSV"
+    echo "$i,$MATRIX_SIZE,$REAL_TIME,$USER_TIME,$SYS_TIME"
 done
 
 rm tempo.txt
